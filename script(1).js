3ff9c443ef4bbc13f5b2e5057c90a53591662a1e
Blessedadudu <adududuke@gmail.com>
2020-05-08 13:04:26 +0100

    update on script

--- script(1).js
+++ script(1).js
@@ -1,34 +1,34 @@
-// var css = document.querySelector("h3");
-// var color1 = document.querySelector(".color1");
-// var color2 = document.querySelector(".color2");
-// var body = document.getElementById("gradient");
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
 
-// function setGradient() {
-// 		body.style.background = 
-// 		"linear-gradient(to right, " + color1.value +
-// 		", " + color2.value + ")";
+function setGradient() {
+		body.style.background = 
+		"linear-gradient(to right, " + color1.value +
+		", " + color2.value + ")";
 
-// 		css.innerHTML = body.style.background + ",";
-// };
+		css.innerHTML = body.style.background + ",";
+};
 
-// color1.addEventListener("input", setGradient);
-// color2.addEventListener("input", setGradient)
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient)
 
 
 
-// 	css.textContent = body.style.background + ";";
+css.textContent = body.style.background + ";";
+
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+// var a = prompt("firstNumber")
+
+// var b = prompt("secondNumber")
+// function multiply(a, b) {
+// 	return a*b;
 // }
-
-// color1.addEventListener("input", setGradient);
-
-// color2.addEventListener("input", setGradient);
-
-var a = prompt("firstNumber")
-
-var b = prompt("secondNumber")
-function multiply(a, b) {
-	return a*b;
-}
-// prompt (a);
-// prompt (b);
-alert(multiply(a,b))
+// // prompt (a);
+// // prompt (b);
+// alert(multiply(a,b))
